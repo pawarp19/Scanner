8e63fc7bf7bbda466601392f0b26aff33b839525
Kajal Pawar <kajalpawar2382001@gmail.com>
2024-07-31 00:02:36 +0530

    cors issue

--- functions/balance.js
+++ functions/balance.js
@@ -35,6 +35,11 @@ exports.handler = async (event) => {
     const balance = await fetchBulkSmsBalance();
     return {
       statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+      },
       body: JSON.stringify({ balance }),
     };
   } catch (error) {
